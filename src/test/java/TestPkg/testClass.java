995166498be3cb0896977f22546f92106ce50c1c
amit.kumar <akumar46@apple.com>
2019-05-10 17:05:52 +0530

    uploading complete project

---
+++ src/test/java/TestPkg/testClass.java
@@ -0,0 +1,10 @@
+package TestPkg;
+
+public class testClass {
+
+	public static void main(String[] args) {
+		System.out.println("test class");
+
+	}
+
+}
---
+++ src/test/java/TestPkg/testClass2.java
@@ -0,0 +1,14 @@
+package TestPkg;
+
+import org.testng.annotations.Test;
+
+public class testClass2 {
+
+	
+	@Test
+	public static void test(){
+		System.out.println("test class2");
+
+	}
+
+}
