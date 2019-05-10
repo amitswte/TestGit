c5b6c0eeef4061c1ede14d6c04d1ed2ebf9aee95
amit.kumar <akumar46@apple.com>
2019-05-10 17:49:06 +0530

    updating test class

--- src/test/java/TestPkg/testClass.java
+++ src/test/java/TestPkg/testClass.java
@@ -3,6 +3,12 @@ package TestPkg;
 public class testClass {
 
 	public static void main(String[] args) {
+		
+		String st1="test";
+		System.out.println("testline1");
+		
+		System.out.println("testline1");
+		
 		System.out.println("test class");
 
 	}
