f66ac02f16ec7b3cc639e45077cef192e78d427e
amit.kumar <akumar46@apple.com>
2019-05-14 19:19:53 +0530

    2nd commit

--- src/test/java/TestPkg/testClass.java
+++ src/test/java/TestPkg/testClass.java
@@ -4,13 +4,9 @@ public class testClass {
 
 	public static void main(String[] args) {
 		
+		String st2="test2";
 		String st1="testABCD";
-		System.out.println("testline1");
 		
-		System.out.println("testline1");
-		
-		System.out.println("test class");
-
 	}
 
 }
